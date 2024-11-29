4e06a0ec5fcbee48a0a07cfe80976e4dfd6f094a
Maks--18 <m.v.besedin@student.khai.edu>
2024-11-29 16:19:35 +0200

    ChatBot 5th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -18,3 +18,22 @@ def chatbot():
     for i in range(num + 1):
         print(f"{i} !")
 
+    print("Let's test your programming knowledge.")
+    while True:
+        print("Why do we use methods?")
+        print("1. To repeat a statement multiple times.")
+        print("2. To decompose a program into several small subroutines.")
+        print("3. To determine the execution time of a program.")
+        print("4. To interrupt the execution of a program.")
+        answer = input("> ")
+        if answer == "2":
+            print("Completed, have a nice day!")
+            break
+        else:
+            print("Please, try again.")
+
+    print("Congratulations, have a nice day!")
+
+
+if __name__ == "__main__":
+    chatbot()
