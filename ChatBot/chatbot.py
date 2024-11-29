d7440e49f4f75c86150908fab41939a69f568807
Maks--18 <m.v.besedin@student.khai.edu>
2024-11-29 16:08:26 +0200

    ChatBot 2nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,4 +1,7 @@
+def chatbot():
+    print("Hello! My name is DICT_Bot.")
+    print("I was created 2024.")
 
+if __name__ == "__main__":
+    chatbot()
 
-print("Hello! My name is DICT_Bot.")
-print("I was created in 2024")
